7244c8376a854811254d1e23ec94df7ca9ebae4b
Roll <quek.ruoling@gmail.com>
2020-05-21 22:25:23 +0800

    [MISC] [Roll] Pass error callback for debugging

--- src/util/RequestUtil.ts
+++ src/util/RequestUtil.ts
@@ -1,4 +1,4 @@
-import * as AWS from "aws-sdk"
+import * as AWS from "aws-sdk";
 
 /**
  * Send the request to AWS
@@ -11,23 +11,30 @@ export function sendRequest(request): Promise<any> {
     // [DY] No typescript declaration for NodeHttpClient: https://github.com/aws/aws-sdk-js/issues/1729
     const send = new (AWS as any).NodeHttpClient();
     return new Promise((resolve, reject) => {
-        send.handleRequest(request, null, function (response) {
-            let respBody = "";
-            response.on("data", function (chunk) {
-                respBody += chunk;
-            });
-            response.on("end", function () {
-                if (isForbiddenRequestOrServerError(response)) {
-                    reject(new Error(respBody));
-                }
-                try {
-                    resolve(JSON.parse(respBody));
-                } catch (e) {
-                    reject(new Error(respBody));
-                }
-            });
-        });
-    })
+        send.handleRequest(
+            request,
+            null,
+            function (response) {
+                let respBody = "";
+                response.on("data", function (chunk) {
+                    respBody += chunk;
+                });
+                response.on("end", function () {
+                    if (isForbiddenRequestOrServerError(response)) {
+                        reject(new Error(respBody));
+                    }
+                    try {
+                        resolve(JSON.parse(respBody));
+                    } catch (e) {
+                        reject(new Error(respBody));
+                    }
+                });
+            },
+            function (err) {
+                reject(err);
+            }
+        );
+    });
 }
 
 function isForbiddenRequestOrServerError(response) {
